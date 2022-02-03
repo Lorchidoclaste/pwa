584c6c1d019af0d01ee270979c49c2a6e8eb6a2f
Lorchidoclaste <samuel.niefer@gmail.com>
2022-02-03 18:41:09 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ self.addEventListener('install', (e) => {
     e.waitUntil(
       caches.open('v1').then((cache) => {
         return cache.addAll([
+            'favicon.ico',
             'index.html',
             'index.js',
             'icone-192x192.png',
